0237cf2ce84c5a87cf13fd420880f011e958a5f7
Grisha <grisha765@tuta.io>
2025-05-23 20:33:42 +0300

    [u] reload & [f] fic bool

--- appeals/config/config.py
+++ appeals/config/config.py
@@ -2,6 +2,7 @@ import os
 
 class Config:
     log_level: str = "INFO"
+    reload: bool = False
     api_host: str = '127.0.0.1'
     api_port: int = 8000
     db_path: str = 'data.db'
@@ -12,7 +13,9 @@ class Config:
             env_value = os.getenv(key.upper())
             if env_value is not None:
                 current_value = getattr(cls, key)
-                if isinstance(current_value, int):
+                if isinstance(current_value, bool):
+                    setattr(cls, key, env_value.lower() in ('true', '1', 'yes'))
+                elif isinstance(current_value, int):
                     setattr(cls, key, int(env_value))
                 elif isinstance(current_value, float):
                     setattr(cls, key, float(env_value))
